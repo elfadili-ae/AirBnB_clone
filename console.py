e50cd1b178ad5dac154f737177bb46d8f156c7d0
zrdouane <ramirdouane@gmail.com>
2023-08-06 14:40:24 +0100

    update cmd interpreter && add create and save it into json.file && handle the missing class name print ** class name missing ** && handle the class name doesn't exist print ** class doesn't exist

--- console.py
+++ console.py
@@ -1,11 +1,24 @@
 #!/usr/bin/python3
 """Console's Module"""
 import cmd
+from models import storage
+import re
+from shlex import split
+from models.base_model import BaseModel
+
 
 
 class HBNBCommand(cmd.Cmd):
     prompt = "(hbnb) "
-
+    __classes = {
+        "BaseModel",
+        "User",
+        "State",
+        "City",
+        "Place",
+        "Amenity",
+        "Review"
+    }
 
     # def do_help(self, arg):
     #     """help command"""
@@ -24,5 +37,32 @@ class HBNBCommand(cmd.Cmd):
     def emptyline(self):
         """ overwriting the emptyline method """
         pass
+    def do_create(self, arg):
+        """Creates a new instance of BaseModel , saves it (to the JSON file) """
+        argv = parsing(arg)
+        if len(argv) == 0:
+            print("** class name missing **")
+        elif argv[0] not in HBNBCommand.__classes:
+            print("** class doesn't exist **")
+        else:
+            print(eval(argv[0])().id)
+            storage.save()
+
+def parsing(arg):
+    brace = re.search(r"\{(.*?)\}", arg)
+    bracket = re.search(r"\[(.*?)\]", arg)
+    if brace is None:
+        if bracket is None:
+            return [i.strip(",") for i in split(arg)]
+        else:
+            lxr = split(arg[:bracket.span()[0]])
+            ret = [i.strip(",") for i in lxr]
+            ret.append(bracket.group())
+            return ret
+    else:
+        lxr = split(arg[:brace.span()[0]])
+        ret = [i.strip(",") for i in lxr]
+        ret.append(brace.group())
+        return ret            
 if __name__ == '__main__':
     HBNBCommand().cmdloop()
